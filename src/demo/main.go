6e5bba0c873606e6123543a1b01beac9e536b0a2
Marc van Gorp <Marc.vanGorp@co-one.com>
2026-06-30 10:01:11 +0000

    converted to struct

--- src/demo/main.go
+++ src/demo/main.go
@@ -14,15 +14,31 @@ func main() {
 
   // name, score := "Dent, Arthur", 87
 
-  students := []string{"Dent, Arthur",
-    "MacMillan, Tricia",
-    "Ford, Prefect",
+  // students := []string{"Dent, Arthur",
+  //   "MacMillan, Tricia",
+  //   "Ford, Prefect",
+  // }
+  // scores := []int{87, 96, 64}
+  // scores := map[string]int{
+  //   students[0]: 87,
+  //   students[1]: 96,
+  //   students[2]: 64,
+  // }
+
+  type score struct {
+    name  string
+    score int
   }
-  scores := []int{87, 96, 64}
+
+  scores := []score{
+    {name: "Dent, Arthur", score: 87},
+    {name: "MacMillan, Tricia", score: 96},
+    {name: "Ford, Prefect", score: 64},
+  }
 
   fmt.Println("Student scores")
   fmt.Println(strings.Repeat("-", 14))
-  fmt.Println(students[0], scores[0])
-  fmt.Println(students[1], scores[1])
-  fmt.Println(students[2], scores[2])
+  fmt.Println(scores[0].name, scores[0].score)
+  fmt.Println(scores[1].name, scores[1].score)
+  fmt.Println(scores[2].name, scores[2].score)
 }
