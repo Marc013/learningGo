c667d64fb52d9447a85a9eeee9e213bcfe833cbc
Marc van Gorp <Marc.vanGorp@co-one.com>
2026-07-07 10:47:55 +0000

    Added channels

--- src/demo/main.go
+++ src/demo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"sync"
@@ -9,13 +10,38 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	var receivedOrdersCh = make(chan order)
+	var validOrderCh = make(chan order)
+	var invalidOrderCh = make(chan invalidOrder)
+
+	go receiveOrders(receivedOrdersCh)
+	go validateOrders(receivedOrdersCh, validOrderCh, invalidOrderCh)
+
 	wg.Add(1)
-	go receiveOrders(&wg)
+	go func() {
+		order := <-validOrderCh
+		fmt.Printf("Valid order received: %v\n", order)
+		wg.Done()
+	}()
+	go func() {
+		order := <-invalidOrderCh
+		fmt.Printf("Invalid order received: %v, Issue: %v\n", order.order, order.err)
+		wg.Done()
+	}()
 	wg.Wait()
-	fmt.Println(orders)
 }
 
-func receiveOrders(wg * sync.WaitGroup) {
+func validateOrders(in, out chan order, errCh chan invalidOrder) {
+	order := <-in
+	if order.Quantity <= 0 {
+		errCh <- invalidOrder{order: order, err:
+			errors.New("quantity must be greater than zero")}
+	} else {
+		out <- order
+	}
+}
+
+func receiveOrders(out chan order) {
 	for _, rawOrder := range rawOrders {
 		var newOrder order
 		err := json.Unmarshal([]byte(rawOrder), &newOrder)
@@ -23,14 +49,13 @@ func receiveOrders(wg * sync.WaitGroup) {
 			log.Print(err)
 			continue
 		}
-		orders = append(orders, newOrder)
+		out <- newOrder
 	}
-	wg.Done()
 }
 
 var rawOrders = []string{
-	`{"productCode": 1111, "Quantity": 5, "status": 1}`,
-	`{"productCode": 2222, "Quantity": 42.3, "status": 1}`,
-	`{"productCode": 3333, "Quantity": 19, "status": 1}`,
-	`{"productCode": 4444, "Quantity": 8, "status": 1}`,
+	`{"ProductCode": 1111, "Quantity": 5, "Status": 1}`, // to make this fail, change "5" to "-5"
+	`{"ProductCode": 2222, "Quantity": 42.3, "Status": 1}`,
+	`{"ProductCode": 3333, "Quantity": 19, "Status": 1}`,
+	`{"ProductCode": 4444, "Quantity": 8, "Status": 1}`,
 }
